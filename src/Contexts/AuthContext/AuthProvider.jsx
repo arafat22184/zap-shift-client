1768fb036b5985345ee7952b73a7c3cf4ddd854b
arafat22184 <123alarafat@gmail.com>
2025-06-21 03:59:09 +0600

    added firebase authContext and authProvider

--- src/Contexts/AuthContext/AuthProvider.jsx
+++ src/Contexts/AuthContext/AuthProvider.jsx
@@ -1,7 +1,44 @@
+import { useEffect, useState } from "react";
+import { auth } from "../../Firebase/firebase.init";
 import { AuthContext } from "./AuthContext";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
 
 const AuthProvider = ({ children }) => {
-  const authInfo = {};
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+
+    return () => {
+      unSubscribe();
+    };
+  }, []);
+
+  const authInfo = { createUser, signIn, logOut, user, loading };
   return <AuthContext value={authInfo}>{children}</AuthContext>;
 };
 
