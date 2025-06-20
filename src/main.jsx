26dea0fe1d0532c3b9231949e0f53da134bed1f7
arafat22184 <123alarafat@gmail.com>
2025-06-21 03:50:17 +0600

    added register page

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,15 @@ import { RouterProvider } from "react-router";
 import { router } from "./Router/router";
 import "aos/dist/aos.css";
 import Aos from "aos";
+import AuthProvider from "./Contexts/AuthContext/AuthProvider";
 Aos.init();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <div className="font-urbanist max-w-7xl mx-auto">
-      <RouterProvider router={router}></RouterProvider>
+      <AuthProvider>
+        <RouterProvider router={router}></RouterProvider>
+      </AuthProvider>
     </div>
   </StrictMode>
 );
