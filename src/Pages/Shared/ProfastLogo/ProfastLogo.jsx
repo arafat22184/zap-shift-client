2b0b70c85e0c46513906ce4755ca2947108f7017
arafat22184 <123alarafat@gmail.com>
2025-06-21 20:35:04 +0600

    added coverage map

--- src/Pages/Shared/ProfastLogo/ProfastLogo.jsx
+++ src/Pages/Shared/ProfastLogo/ProfastLogo.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import logo from "../../../assets/logo.png";
+import { Link } from "react-router";
 
 const ProfastLogo = () => {
   return (
-    <div className="flex items-end">
-      <img className="mb-2" src={logo} alt="logo" />
-      <p className="-ml-2 text-3xl font-extrabold">ProFast</p>
-    </div>
+    <Link to={"/"}>
+      <div className="flex items-end">
+        <img className="mb-2" src={logo} alt="logo" />
+        <p className="-ml-2 text-3xl font-extrabold">ProFast</p>
+      </div>
+    </Link>
   );
 };
 
--- src/Router/router.jsx
+++ src/Router/router.jsx
@@ -4,6 +4,7 @@ import Home from "../Pages/Home/Home/Home";
 import AuthLayout from "../Layouts/AuthLayout";
 import Login from "../Pages/Authentication/Login/Login";
 import Register from "../Pages/Authentication/Register/Register";
+import Coverage from "../Pages/Coverage/Coverage";
 
 export const router = createBrowserRouter([
   {
@@ -13,6 +14,11 @@ export const router = createBrowserRouter([
       {
         index: true,
         Component: Home,
+      },
+      {
+        path: "/coverage",
+        Component: Coverage,
+        loader: () => fetch("./serviceCenter.json"),
       },
     ],
   },
