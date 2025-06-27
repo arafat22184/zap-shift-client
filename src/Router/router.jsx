583bd8ddc53eb4fbb038078a446821e1950ef8df
arafat22184 <123alarafat@gmail.com>
2025-06-27 20:33:34 +0600

    payment method integrated

--- src/Router/router.jsx
+++ src/Router/router.jsx
@@ -9,6 +9,7 @@ import PrivateRoute from "../Routes/PrivateRoute";
 import SendParcel from "../Pages/SendParcel/SendParcel";
 import DashboardLayout from "../Layouts/DashboardLayout";
 import MyParcels from "../Pages/Dashboard/MyParcels/MyParcels";
+import Payment from "../Pages/Dashboard/Payment/Payment";
 
 export const router = createBrowserRouter([
   {
@@ -60,6 +61,10 @@ export const router = createBrowserRouter([
       {
         path: "myParcels",
         Component: MyParcels,
+      },
+      {
+        path: "payment/:parcelId",
+        Component: Payment,
       },
     ],
   },
